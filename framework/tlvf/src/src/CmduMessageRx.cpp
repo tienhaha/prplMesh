a0e2ef9779d35bb577db6ac000721981e2b80f0d
Ran Regev <ran.regev@devalore.com>
2020-05-11 21:58:53 +0300

    tlvf: added beacon metrics response to cmdu rx message
    Signed-off-by: Ran Regev <ran.regev@devalore.com>

--- framework/tlvf/src/src/CmduMessageRx.cpp
+++ framework/tlvf/src/src/CmduMessageRx.cpp
@@ -32,6 +32,7 @@
 #include <tlvf/wfa_map/tlvApMetricQuery.h>
 #include <tlvf/wfa_map/tlvApRadioBasicCapabilities.h>
 #include <tlvf/wfa_map/tlvApRadioIdentifier.h>
+#include <tlvf/wfa_map/tlvBeaconMetricsResponse.h>
 #include <tlvf/wfa_map/tlvChannelPreference.h>
 #include <tlvf/wfa_map/tlvChannelSelectionResponse.h>
 #include <tlvf/wfa_map/tlvClientAssociationControlRequest.h>
@@ -202,6 +203,9 @@ std::shared_ptr<BaseClass> CmduMessageRx::parseNextTlv()
     }
     case (149): {
         return msg.addClass<wfa_map::tlvStaMacAddressType>();
+    }
+    case (154): {
+        return msg.addClass<wfa_map::tlvBeaconMetricsResponse>();
     }
     case (155): {
         return msg.addClass<wfa_map::tlvSteeringRequest>();
